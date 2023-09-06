a4e28610e8f05a2155c6b9d257277d8555e57cd3
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-09-05 22:08:37 -0300

    add navbar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
       hola mundo
-    </div>
+    </>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,33 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./NavBar.css";
 
-const NavBar = () => {
-  return <nav>hola soy un NavBar</nav>;
-};
+function BasicExample() {
+  return (
+    <Navbar fixed="top" expand="lg" className={["bg-body-tertiary, navbar"]}>
+      <Container>
+        {/* logo */}
+        <Navbar.Brand href="#home">Esto no es Moda</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        {/* cartwidget */}
+      </Container>
+    </Navbar>
+  );
+}
 
-export default NavBar;
+export default BasicExample;
