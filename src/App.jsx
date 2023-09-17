12c9825b9cf04e12b16e6b5b352b27bfc0a8736f
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-09-17 17:53:40 -0300

    install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ItemListContainer greeting="Bienvenidx a Esto no es Moda👠" />,
+  },
+]);
 
 function App() {
   return (
     <>
       <NavBar />
-      <ItemListContainer greeting="Bienvenidx a Esto no es Moda👠" />
+      <RouterProvider router={router} />
     </>
   );
 }
