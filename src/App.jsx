8eeb55b70f3abd9ad3d1e7070da77363904cce28
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-09-17 20:51:09 -0300

    create basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <ItemListContainer greeting="Bienvenidx a Esto no es Moda👠" />,
+  },
+  {
+    path: "/category/:id",
+    element: <ItemListContainer />,
+  },
+  {
+    path: "/item/:id",
+    element: <ItemDetailContainer />,
   },
 ]);
 
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,11 @@
+import "./ItemDetailContainer.css";
+
+function ItemDetailContainer() {
+  return (
+    <div>
+      <h1>Detalle del producto</h1>
+    </div>
+  );
+}
+
+export default ItemDetailContainer;
