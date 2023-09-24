0b24b234f02cf705984f8c6341dc194b1d6d82c8
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-09-24 13:40:08 -0300

    add item and itemlist components

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,22 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import "./Item.css";
+
+const Item = ({ product }) => {
+  return (
+    <Card className="cardStyle" key={product.id}>
+      <Card.Img className="img" variant="top" src={`/images/${product.img}`} />
+      <Card.Body>
+        <Card.Title className="productTitle">{product.title}</Card.Title>
+        <Link to={`/item/${product.id}`}>
+          <Button className="buttonDetails" variant="primary">
+            See details
+          </Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Item;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,10 +3,8 @@ import { useEffect, useState } from "react";
 import "./ItemListContainer.css";
 import { useParams } from "react-router-dom";
 import { getAllProducts, getProductsByCategory } from "../../api/products";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
 import { Layout } from "../Layout";
+import { ItemList } from "../ItemList";
 
 function ItemListContainer(props) {
   const { id: categoryId } = useParams();
@@ -31,21 +29,7 @@ function ItemListContainer(props) {
         <div className="welcome">
           <h1 className="greeting">{props.greeting}</h1>
         </div>
-        <div className="itemlist">
-          {products.map((product) => (
-            <Card className="cardStyle" key={product.id}>
-              <Card.Img className="img" variant="top" src={`/images/${product.img}`} />
-              <Card.Body>
-                <Card.Title className="productTitle">{product.title}</Card.Title>
-                <Link to={`/item/${product.id}`}>
-                  <Button className="buttonDetails" variant="primary">
-                    See details
-                  </Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
+        <ItemList products={products} />
       </div>
     </Layout>
   );
