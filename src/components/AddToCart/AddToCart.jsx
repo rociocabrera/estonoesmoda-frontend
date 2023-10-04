7c25f14f0eb5a86a01422af69dfa837dea455ec8
Rocio Cabrera <rocioevelincabrera@gmail.com>
2023-10-04 16:01:01 -0300

    creating add to cart component

---
+++ src/components/AddToCart/AddToCart.jsx
@@ -0,0 +1,11 @@
+import "./AddToCart.css";
+
+export const AddToCart = () => {
+  return (
+    <div className="addToCart">
+      <button className="addToCartButton">Add to cart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,6 +2,7 @@ import Card from "react-bootstrap/Card";
 import "./ItemDetail.css";
 import { Counter } from "../Counter";
 import { Loader } from "../Loader";
+import { AddToCart } from "../AddToCart";
 
 export const ItemDetail = (props) => {
   const { product, loading } = props;
@@ -9,16 +10,17 @@ export const ItemDetail = (props) => {
   return loading ? (
     <Loader />
   ) : product ? (
-    <div>
+    <div className="cardItemDetail">
       <Card className="cardProduct" key={product.id}>
         <Card.Img className="imgProduct" variant="top" src={`/images/${product.img}`} />
-        <span className="cardContent">
+        <div className="cardDetail">
           <h2>{product.name}</h2>
-          <p className="productParagraph">ID: {product.id}</p>
-          <p className="productParagraph">Price: ${product.price}</p>
-          <p className="productParagraph">Description: {product.title}</p>
+          <p className="productParagraph">{product.title}</p>
+          <p className="productParagraph">${product.price}</p>
+          {/* add product description */}
           <Counter />
-        </span>
+          <AddToCart />
+        </div>
       </Card>
     </div>
   ) : (
